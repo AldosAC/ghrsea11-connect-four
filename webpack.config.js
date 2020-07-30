8a9ebce749573937e0a2038ed9a3becda0a3adc2
Joel Carpenter <aldosac@gmail.com>
2020-07-30 09:49:30 -0700

    Install and setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path require('path');
+
+const SRC_DIR = path.resolve(__dirname, "client", "src");
+const SRC_FILE = path.resolve(SRC_DIR, "index.js");
+const OUT_DIR = path.resolve(__dirname, "public");
+
+module.exports={
+  entry: SRC_FILE,
+  output: {
+    filename: 'bundle.js',
+    path: OUT_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
